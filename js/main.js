ce11589d16c31e9e8d0bd019951104bde6bef3b8
sazalahmed <sazal.web@gmail.com>
2025-10-14 17:30:07 +0600

    blog added and footer started

--- js/main.js
+++ js/main.js
@@ -195,7 +195,7 @@ $(function () {
     });
 
 
-    // volunteer_slider Slider
+    // volunteer slider Slider
     $('.volunteer_slider').slick({
         slidesToShow: 4,
         slidesToScroll: 1,
@@ -233,6 +233,45 @@ $(function () {
     });
 
 
+
+    // brand Slider
+    $('.brand_slider').slick({
+        slidesToShow: 5,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        dots: false,
+        arrows: true,
+        nextArrow: '<i class="far fa-arrow-right nextArrow"></i>',
+        prevArrow: '<i class="far fa-arrow-left prevArrow"></i>',
+
+        responsive: [
+            {
+                breakpoint: 1200,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 992,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 768,
+                settings: {
+                    slidesToShow: 2,
+                }
+            },
+            {
+                breakpoint: 576,
+                settings: {
+                    slidesToShow: 1,
+                }
+            }
+        ]
+    });
 
 
 
