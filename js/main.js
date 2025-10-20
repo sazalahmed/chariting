4dc256737848cd698923597e2311640e06ab2d93
sazalahmed <sazal.web@gmail.com>
2025-10-20 16:14:46 +0600

    three pages responsive done

--- js/main.js
+++ js/main.js
@@ -328,6 +328,7 @@ $(function () {
                 breakpoint: 576,
                 settings: {
                     slidesToShow: 1,
+                    slidesToScroll: 1,
                 }
             }
         ]
