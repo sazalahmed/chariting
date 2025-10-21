2d62fb2f4bbb89184a355be97639293f622b9b4f
sazalahmed <sazal.web@gmail.com>
2025-10-21 14:12:27 +0600

    seven pages responsive done

--- js/main.js
+++ js/main.js
@@ -309,19 +309,22 @@ $(function () {
             {
                 breakpoint: 1200,
                 settings: {
-                    slidesToShow: 3,
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
                 }
             },
             {
                 breakpoint: 992,
                 settings: {
                     slidesToShow: 2,
+                    slidesToScroll: 2,
                 }
             },
             {
                 breakpoint: 768,
                 settings: {
-                    slidesToShow: 2,
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
                 }
             },
             {
